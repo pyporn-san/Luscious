f07e7a3e9b22ee3a05acd6beee5688ca6734af86
pyporn-san <pypornsan@gmail.com>
2021-03-10 16:29:21 +0330

    Implemented RequestHandler

--- luscious/luscious.py
+++ luscious/luscious.py
@@ -1,8 +1,92 @@
 from dataclasses import dataclass
+from functools import cached_property
+from typing import List, Tuple
+from urllib.parse import urljoin
+from urllib.request import getproxies
 
+import requests
+from faker import Faker
+from pathvalidate import sanitize_filepath
+from requests import Session
+from requests.adapters import HTTPAdapter
+from requests.models import Response
+from urllib3.util.retry import Retry
 
 class RequestHandler(object):
-    pass
+    """
+    RequestHandler
+    ==============
+    Defines a synchronous request handler class that provides methods and
+    properties for working with REST APIs that is backed by the `requests`
+    library.
+    """
+    _timeout = (5, 5)
+    _total = 5
+    _status_forcelist = [413, 429, 500, 502, 503, 504]
+    _backoff_factor = 1
+    _fake = Faker()
+
+    def __init__(self,
+                 timeout: Tuple[float, float] = _timeout,
+                 total: int = _total,
+                 status_forcelist: List[int] = _status_forcelist.copy(),
+                 backoff_factor: int = _backoff_factor):
+        """Instantiates a new request handler object."""
+        self.timeout = timeout
+        self.total = total
+        self.status_forcelist = status_forcelist
+        self.backoff_factor = backoff_factor
+
+    @cached_property
+    def retry_strategy(self) -> Retry:
+        """
+        The retry strategy returns the retry configuration made up of the
+        number of total retries, the status forcelist as well as the backoff
+        factor. It is used in the session property where these values are
+        passed to the HTTPAdapter.
+        """
+        return Retry(total=self.total,
+                     status_forcelist=self.status_forcelist,
+                     backoff_factor=self.backoff_factor
+                     )
+
+    @cached_property
+    def session(self) -> Session:
+        """
+        Creates a custom session object. A request session provides cookie
+        persistence, connection-pooling, and further configuration options
+        that are exposed in the RequestHandler methods in form of parameters
+        and keyword arguments.
+        """
+        assert_status_hook = lambda response, * \
+            args, **kwargs: response.raise_for_status()
+        session = requests.Session()
+        session.mount("https://", HTTPAdapter(max_retries=self.retry_strategy))
+        session.hooks['response'] = [assert_status_hook]
+        session.headers.update({
+            "User-Agent": RequestHandler._fake.chrome(version_from=80, version_to=86, build_from=4100, build_to=4200)
+        })
+        return session
+
+    def get(self, url: str, params: dict = None, **kwargs) -> Response:
+        """
+        Returns the GET request encoded in `utf-8`. Adds proxies to this session
+        on the fly if urllib is able to pick up the system's proxy settings.
+        """
+        response = self.session.get(
+            url, timeout=self.timeout, params=params, proxies=getproxies(), **kwargs)
+        response.encoding = 'utf-8'
+        return response
+
+    def post(self, url: str, params: dict = None, **kwargs) -> Response:
+        """
+        Returns the POST request encoded in `utf-8`. Adds proxies to this session
+        on the fly if urllib is able to pick up the system's proxy settings.
+        """
+        response = self.session.post(
+            url, timeout=self.timeout, params=params, proxies=getproxies(), **kwargs)
+        response.encoding = 'utf-8'
+        return response
 
 
 @dataclass
