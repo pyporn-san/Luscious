be9172a9e313dc67a6f6295f07e3c02cc2b0cbeb
pyporn-san <pypornsan@gmail.com>
2021-03-10 16:21:14 +0330

    Initial commit

---
+++ luscious/luscious.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+
+class RequestHandler(object):
+    pass
+
+
+@dataclass
+class Tag():
+    pass
+
+
+class Album():
+    pass
+
+
+class Luscious(RequestHandler):
+    pass
